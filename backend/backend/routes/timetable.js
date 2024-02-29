475dc6143489bc1651c5f9c01f3cf9b72a557669
hamim-87 <hamim12109087@gmail.com>
2024-03-01 03:58:37 +0600

    timetable is done

--- backend/backend/routes/timetable.js
+++ backend/backend/routes/timetable.js
@@ -15,37 +15,34 @@ router.use(bodyParser.urlencoded({ extended: true }));
 
 router.use(bodyParser.json());
 
-router.get('/', (req, res) =>
-{
-    const TimeTablePath = path.join(__dirname, '..','..','Frontend','timetable.html');
-    console.log(TimeTablePath);
-
-    res.sendFile(TimeTablePath);
-});
 
 router.post('/',async (req, res) =>{
 
 
-    //console.log(req.body);
-    let from = req.body.enteredStation;
+     console.log(req.body);
+    const from = req.body.source;
+    const to = req.body.destination;
     
 
     console.log(from);
+    console.log(to);
     
     const sql = `
-    SELECT time
-    FROM TIME_TABLE T JOIN STATION S ON T.STATION_ID=S.STATION_ID AND T.DIRECTION=S.PLATFORM
-    WHERE T.Time >= REPLACE( TO_CHAR(SYSDATE, 'HH24:MI'),':','') AND S.STATION_NAME='${from}' AND S.PLATFORM=1
-    ORDER BY TIME
+    SELECT TO_CHAR(T.TIME)
+FROM TIME_TABLE T JOIN STATION S ON T.STATION_ID=S.STATION_ID AND T.DIRECTION=S.PLATFORM
+WHERE T.Time >= TO_NUMBER(REPLACE( TO_CHAR(SYSDATE, 'HH24:MI'),':','')) AND S.STATION_ID=(SELECT STATION_ID 
+	  																																														FROM STATION 
+																																																WHERE STATION_NAME='${from}' AND PLATFORM=1)
+AND S.PLATFORM=DIRECTION_STATUS('${from}','${to}')
+ORDER BY TIME
     `;
-    let motijhilend,uttoraend;
+    
     try{
         const result = await execute(sql,[]);
-        console.log(result.rows);
-        motijhilend = result.rows;
+        
 
-        //returning to the client
-        //res.send(result.rows);
+      
+        res.send(result.rows);
         
 
     }catch(err){
@@ -54,35 +51,7 @@ router.post('/',async (req, res) =>{
         console.error(err);
     }
 
-    const sql2 = `
-    SELECT time
-    FROM TIME_TABLE T JOIN STATION S ON T.STATION_ID=S.STATION_ID AND T.DIRECTION=S.PLATFORM
-    WHERE T.Time >= REPLACE( TO_CHAR(SYSDATE, 'HH24:MI'),':','') AND S.STATION_NAME='${from}' AND S.PLATFORM=2
-    ORDER BY TIME
-    `;
-
-    try{
-        const result = await execute(sql2,[]);
-        console.log(result.rows);
-
-        //returning to the client
-        //res.send(result.rows);
-
-        uttoraend = result.rows;
-        
-
-    }catch(err){
-        
-        
-        console.error(err);
-    }
-
-    const combinedJson = {
-        Motijheel_End: motijhilend,
-        Utarra_End: uttoraend
-    };
-
-    res.send(combinedJson);
+    
     
 
     
