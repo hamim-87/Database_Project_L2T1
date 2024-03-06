055bc8ad1e91e1a9eb0f077b2a7e312ecfcb560f
hamim-87 <hamim12109087@gmail.com>
2024-03-06 10:35:19 +0600

    bakend trip created

--- backend/backend/routes/updatetrip.js
+++ backend/backend/routes/updatetrip.js
@@ -67,7 +67,43 @@ router.post('/', async (req, res) => {
     const result = await execute(sql,binds,options);
     console.log("sql res");
     console.log(result);
-    res.send({massage: "done"});
+
+    const sql2 = `
+    SELECT (TRUNC(TIME/100) || ':' || SUBSTR(TO_CHAR(TIME), LENGTH(TO_CHAR(TIME))-1)) AS "Time"
+FROM METRO_ROUTE
+WHERE DIRECTION=DIRECTION_STATUS('${from}','${to}') AND STATION_ID=(SELECT STATION_ID
+                                                                    FROM STATION 
+																    WHERE STATION_NAME = '${to}' AND PLATFORM = 1)
+ AND 
+ METRO_NO=(SELECT METRO_NO
+			FROM METRO_ROUTE 
+			WHERE DIRECTION=DIRECTION_STATUS('${from}','${to}') 
+			AND 
+			TIME=(SELECT T.TIME
+			FROM TIME_TABLE T JOIN STATION S ON T.STATION_ID=S.STATION_ID AND T.DIRECTION=S.PLATFORM
+			WHERE T.Time >= TO_NUMBER(REPLACE( TO_CHAR(SYSDATE, 'HH24:MI'),':','')) 
+			AND S.STATION_NAME='${from}'
+		    AND S.PLATFORM=DIRECTION_STATUS('${from}','${to}')
+			ORDER BY TIME
+			FETCH FIRST 1 ROWS ONLY) 
+              AND STATION_ID=(SELECT STATION_ID
+															FROM STATION 
+															WHERE STATION_NAME ='${from}' AND PLATFORM = 1))
+AND TIME>(SELECT T.TIME
+										FROM TIME_TABLE T JOIN STATION S ON T.STATION_ID=S.STATION_ID AND T.DIRECTION=S.PLATFORM
+										WHERE T.Time >= TO_NUMBER(REPLACE( TO_CHAR(SYSDATE, 'HH24:MI'),':','')) 
+										AND S.STATION_NAME='${from}'
+										AND S.PLATFORM=DIRECTION_STATUS('${from}','${to}')
+										ORDER BY TIME
+										FETCH FIRST 1 ROWS ONLY)
+ORDER BY TIME
+FETCH FIRST 1 ROWS ONLY
+    `;
+
+    const result2 = await execute(sql2,[]);
+
+    console.log(result2.rows);
+    res.send(result2.rows);
 
         
     
